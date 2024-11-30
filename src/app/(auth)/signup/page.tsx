a9f5d226b1b4b39fa82f50a355555104cca3e449
theprofessor7 <roland_tc@protonmail.com>
2024-11-30 14:56:40 +0100

    login and register page

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,33 @@
+import { Metadata } from "next";
+import signupImage from "@/assets/signup-image.jpg";
+import Image from "next/image";
+import Link from "next/link";
+import SignUpForm from "./SignUpForm";
+
+export const metadata: Metadata = {
+    title: "Sign Up"
+};
+
+export default function Page() {
+    return (
+        <main className="flex h-screen items-center justify-center p-5">
+            <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] rounded-2xl overflow-hidden bg-card shadow-2xl">
+                <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
+                    <div className="space-y-1 text-center">
+                        <h1 className="text-3xl font-bold">Sign Up to Nitchi</h1>
+                        <p className="text-muted-foreground">
+                            A place where <span className="italic">you</span> can write breathtaking notes and find people with similar interests
+                        </p>
+                    </div>
+                    <div className="space-y-5">
+                        <SignUpForm />
+                        <Link href="/login" className="block text-center hover:underline">
+                            Already have an account? Log in
+                        </Link>
+                    </div>
+                </div>
+                <Image src={signupImage} alt="" className="w-1/2 hidden md:block" />
+            </div>
+        </main>
+    );
+}
